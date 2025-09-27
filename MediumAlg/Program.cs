b7d6cf7e3b855ca9ea9b443155f80d27a4f0229e
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2025-09-26 22:28:59 -0300

    finished

--- MediumAlg/Program.cs
+++ MediumAlg/Program.cs
@@ -1,8 +1,25 @@
 ﻿using MediumAlg;
 
-string s = "abcabcbb";
+#region Legth of Longest Substring
 
+string s = "abcabcbb";
 LengthOfLongestSubstring lengthOfLongestSubstring = new LengthOfLongestSubstring();
 int longestSubstring = lengthOfLongestSubstring.LengthOfLongestSubstringMethod(s);
-Console.WriteLine(longestSubstring);
+
+#endregion
+
+#region Find First And Last Position Of Element In Sorted Array
+
+int[] nums = [5, 7, 7, 8, 8, 10];
+int target = 8;
+
+FindFirstAndLastPositionOfElementInSortedArray testMethod = new FindFirstAndLastPositionOfElementInSortedArray();
+int[] result = testMethod.SearchRange(nums, target);
+
+//result.ToList().ForEach(n => Console.WriteLine(n));
+//Console.WriteLine(string.Join(", ", result));
+
+#endregion
+
+Console.WriteLine(result);
 Console.ReadKey();
