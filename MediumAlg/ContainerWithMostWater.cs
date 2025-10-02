8e99bc0df5c9d2f229a30065166c8d5729f6ad53
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2025-10-02 16:48:08 -0300

    finished

---
+++ MediumAlg/ContainerWithMostWater.cs
@@ -0,0 +1,25 @@
+﻿namespace MediumAlg
+{
+    public static class ContainerWithMostWater
+    {
+        public static int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+            int l = 0; //1,
+            int r = height.Length - 1; //8,7,6
+            while (l < r)
+            {
+                maxArea = Math.Max(maxArea, Math.Min(height[l], height[r]) * (r - l)); //1*8=8 //7*7=49 //3*6=18 MA=49 //5*8=40 MA=49
+                if (height[l] < height[r])
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+            return maxArea;//8,49
+        }
+    }
+}
--- MediumAlg/Program.cs
+++ MediumAlg/Program.cs
@@ -1,30 +1,6 @@
 ﻿using MediumAlg;
 using MediumAlg.Trees;
 
-#region Legth of Longest Substring
-
-string s = "abcabcbb";
-LengthOfLongestSubstring lengthOfLongestSubstring = new LengthOfLongestSubstring();
-int longestSubstring = lengthOfLongestSubstring.LengthOfLongestSubstringMethod(s);
-
-#endregion
-
-#region Find First And Last Position Of Element In Sorted Array
-
-int[] nums = [5, 7, 7, 8, 8, 10];
-int target = 8;
-
-FindFirstAndLastPositionOfElementInSortedArray testMethod = new FindFirstAndLastPositionOfElementInSortedArray();
-int[] result = testMethod.SearchRange(nums, target);
-
-//result.ToList().ForEach(n => Console.WriteLine(n));
-//Console.WriteLine(string.Join(", ", result));
-
-//Console.WriteLine(result);
-//Console.ReadKey();
-
-#endregion
-
 #region InverterArvoreBinaria
 
 TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7, new TreeNode(6), new TreeNode(9)));
@@ -59,11 +35,47 @@ TreeNode root3 = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3
 var tree3 = BreadthFirstSearchTree.LevelOrder(root3);
 //var tree3 = BreadthFirstSearchTree.BreadthFirtsListMethod(root3);
 
-foreach (var nivel in tree3)
-{
-    Console.WriteLine(string.Join(", ", nivel));
-}
+//foreach (var nivel in tree3)
+//{
+//    Console.WriteLine(string.Join(", ", nivel));
+//}
 
-Console.ReadKey();
+//Console.ReadKey();
+
+#endregion
+
+#region Container With Most Water
+
+int[] height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+
+int maxArea = ContainerWithMostWater.MaxArea(height);
+
+//Console.WriteLine(maxArea);
+
+//Console.ReadKey();
+
+#endregion
+
+#region Legth of Longest Substring
+
+string s = "abcabcbb";
+LengthOfLongestSubstring lengthOfLongestSubstring = new LengthOfLongestSubstring();
+int longestSubstring = lengthOfLongestSubstring.LengthOfLongestSubstringMethod(s);
+
+#endregion
+
+#region Find First And Last Position Of Element In Sorted Array
+
+int[] nums = [5, 7, 7, 8, 8, 10];
+int target = 8;
+
+FindFirstAndLastPositionOfElementInSortedArray testMethod = new FindFirstAndLastPositionOfElementInSortedArray();
+int[] result = testMethod.SearchRange(nums, target);
+
+//result.ToList().ForEach(n => Console.WriteLine(n));
+//Console.WriteLine(string.Join(", ", result));
+
+//Console.WriteLine(result);
+//Console.ReadKey();
 
 #endregion
